558f593d3d0a1291d4073ce215614036c4279775
nkpresh <nkwuap@gmail.com>
2022-01-08 12:24:13 +0100

    code update

---
+++ assets/scripts/States/movingState.ts
@@ -0,0 +1,16 @@
+import { _decorator, Component, Node } from 'cc';
+import { ISpawnState } from './ISpawnState';
+const { ccclass, property } = _decorator;
+
+export class movingState implements ISpawnState{
+    EnterState(): void {
+        
+    }
+    UpdateState(): void {
+        
+    }
+    OnCollision(): void {
+        
+    }
+
+}
